59db04131bea889205133e7c1140dac240898651
Amber-Graham <ang190045@gmail.com>
2022-09-15 11:47:59 -0500

    add game welcome

--- main.rb
+++ main.rb
@@ -8,9 +8,22 @@ require 'pry-byebug'
 #close the both files
 
 def game_welcome
-  puts "Welcome to Hangman!"
-  puts "You will have up to 12 guesses to figure out the mystery word!"
-  puts "Would you like to play a new game or continue an old save?"
+  puts "
+  Welcome to Hangman!
+  You will have up to 12 guesses to figure out the mystery word!"
+  game_selection_input = nil
+  until game_selection_input == '1' or game_selection_input == '2'
+    puts "Would you like to play a new game or continue an old save?
+    [1] New Game
+    [2] Saved Game"
+    game_selection_input = gets.chomp
+  end
+  case game_selection_input
+  when '1'
+    game_start
+  when '2'
+    save_games_data
+  end
 end
 
 def get_random_word
@@ -19,5 +32,8 @@ def get_random_word
   word = file[Random.rand(9894)].chomp until word.length.between?(5,12)
   puts word
 end
+
+def game_start
+  
 
 game_welcome
