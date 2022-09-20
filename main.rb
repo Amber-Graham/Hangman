012d3947a3693279d44bd33290e08b4dc562d9e1
Amber-Graham <ang190045@gmail.com>
2022-09-20 17:24:21 -0500

    add YAML configurations

--- main.rb
+++ main.rb
@@ -25,7 +25,7 @@ class Hangman
     when '1'
       game_start
     when '2'
-      if File.exists?()#file name -- will need to look into naming system)
+      if File.exists?()
         load_saved_game
         game_start
       else
@@ -34,19 +34,68 @@ class Hangman
     end
   end
 
-  #def load_saved_game
-    #YAML info here
-    #File.open 
-    # @word = yaml.hash[:word] ==> or something like this to pull the info
-    # do it for each thing we initialize
-  #end
+  def save_game
+    Dir.mkdir 'output' unless Dir.exist? 'output'
+    @filename = "#{random_name}_game.yaml"
+    File.open("output/#{@filename}", "w") { |file| file.write save_to_yaml}
+    puts display_saved_name
+  end
 
-  #def save_game
-    #YAML info here
-    #File.open and would be set to write the info in?
-    # :word = @word
-    # :display_hidden_word = @display_hidden_word 
-  #end
+  def random_name
+    things = %w[cars spy ball baloon map unit bedroom work jellyfish aftermath weather guitar wrist thrill produce patch level opinion operation flame chickens]
+    verbs = %w[like fold inform scab insult warn sip roar praise boast become handle shun sob fish pass forsake purify argue innovate]
+    "#{things[rand(0-20)]}_#{verbs[rand(0-20)]}_#{rand(11..99)}"
+  end
+
+  def save_to_yaml
+    YAML.dump(
+      'word' => @word
+      'guesses_left' => @guesses_left
+      'display_hidden_word' => @display_hidden_word
+      'guesses' => @guesses
+    )
+  end
+
+  def find_saved_file
+    show_file_list
+    file_number = user_input(display_saved_prompt, /\d+|^exit$/)
+    @saved_game = file_list[file_number.to_i -1] unless file_number == 'exit'
+  end
+
+  def show_file_list
+    puts dispay_saved_games('#', 'File Name(s)')
+    file_list.each_with_index do |name, index|
+      puts display_saved_games((index + 1).to_s, name.to_s)
+    end
+  end
+
+  def file_list
+    files = []
+    Dir.entries('output').each do |name|
+      files << name if name.match(/(game)/)
+    end
+    files
+  end
+
+  def load_game
+    find_saved_file
+    load_saved_file
+    player_turns
+    File.delete("output/#{@saved_game}") if File.exist?("output/#{@saved_game}")
+    end_game
+  rescue StandardError
+    puts display_load_error
+  end
+
+  def load_saved_file
+    file - YAML.safe_load(File.read("output/#{@saved_game}"))
+    @word = file['word']
+    @solution = word.split(//)
+    @available_letters = file['available_letters']
+    @solved_letters = file['solved_letters']
+    @incorrect_letters = file['incorrect_letters']
+  end
+end
 
   def game_start
     player_won = false
@@ -102,9 +151,8 @@ class Hangman
       puts "Would you like to play again? (Y/N):\n"
       case gets.strip.upcase
         when "Y"
-          game_welcome
+          Hangman.new
         when "N"
-          puts "See you soon!"
           break
         end
     end
